78027f0dd0b380f03ae4c82eb2d86f81c35ad0f1
Piotr Poręba <poreba__p@wp.pl>
2025-11-25 11:28:01 +0100

    Cleanup

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,21 +17,21 @@ string GetCorrectsign(string playerName)
 const StringComparison stringComparison = StringComparison.OrdinalIgnoreCase;
 
 string player1 = GetCorrectsign("Player 1");
-
 string player2 = GetCorrectsign("Player 2");
 
-int secondsignIndex = allowedSigns.IndexOf(player2);
-
-int winningSignIndex = (secondsignIndex + 1)
+// 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
+int secondSignIndex = allowedSigns.IndexOf(player2);
+// 2. Wylicz indeks znaku, który wygrywa z podanym przez drugą  - wzór - (secondSignIndex + 1) % rozmiarListy
+int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
+// 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
+int firstSignIndex = allowedSigns.IndexOf(player1);
 
 
 if (player1.Equals(player2, stringComparison))
 {
     Console.WriteLine("It's a draw!");
 }
-else if ((player1.Equals(allowedSigns[0], stringComparison) && player2.Equals(allowedSigns[2], stringComparison))
-    ||  (player1.Equals(allowedSigns[1], stringComparison) && player2.Equals(allowedSigns[0], stringComparison))
-    ||  (player1.Equals(allowedSigns[2], stringComparison) && player2.Equals(allowedSigns[1], stringComparison)))
+else if (firstSignIndex == winningSignIndex)
 {
     Console.WriteLine("Player1 won!");
 }
