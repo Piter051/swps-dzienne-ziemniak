c9869a61fee9616af7a3d476511c71e7322c7569
Piotr Poręba <poreba__p@wp.pl>
2025-12-09 10:31:50 +0100

    Small change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -38,33 +38,33 @@ while (true)
     
 
 
-// 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
-int secondSignIndex = allowedSigns.IndexOf(player2);
-// 2. Wylicz indeks znaku, który wygrywa z podanym przez drugą  - wzór - (secondSignIndex + 1) % rozmiarListy
-int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
-// 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
-int firstSignIndex = allowedSigns.IndexOf(player1);
+    // 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
+    int secondSignIndex = allowedSigns.IndexOf(player2);
+    // 2. Wylicz indeks znaku, który wygrywa z podanym przez drugą  - wzór - (secondSignIndex + 1) % rozmiarListy
+    int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
+    // 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
+    int firstSignIndex = allowedSigns.IndexOf(player1);
 
 
-if (player1.Equals(player2, stringComparison))
-{
-    Console.WriteLine("It's a draw!");
-}
-else if (firstSignIndex == winningSignIndex)
-{
-    Console.WriteLine("Player1 won!");
-    player1Score += 1;
-}
-else
-{
-    Console.WriteLine("Player2 won!");
-    player2Score += 1;
-}
-Console.WriteLine($"First player:{player1Score}");
-Console.WriteLine($"Second player:{player2Score}");
+    if (player1.Equals(player2, stringComparison))
+    {
+        Console.WriteLine("It's a draw!");
+    }
+    else if (firstSignIndex == winningSignIndex)
+    {
+        Console.WriteLine("Player1 won!");
+        player1Score += 1;
+    }
+    else
+    {
+        Console.WriteLine("Player2 won!");
+        player2Score += 1;
+    }
+        Console.WriteLine($"First player:{player1Score}");
+        Console.WriteLine($"Second player:{player2Score}");
 
-if (player1Score >= 3 || player2Score >= 3)
-{
-   break;     
-}
+    if (player1Score >= 3 || player2Score >= 3)
+    {
+        break;     
+    }
 }
